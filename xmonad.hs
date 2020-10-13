ee2e483d72979d120bc0442120c53c128755f407
Matt Wittmann <mcwitt@gmail.com>
2020-10-12 18:12:24 -0700

    Add WindowBringer

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,19 @@
+import qualified Data.Map as M
 import XMonad
+import XMonad.Actions.WindowBringer
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
 
 -- modify default layout hook with 'smartBorders'
 myLayoutHook = smartBorders $ layoutHook def
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) =
+  M.fromList
+    [ ((modm .|. shiftMask, xK_g), gotoMenu),
+      ((modm .|. shiftMask, xK_b), bringMenu)
+    ]
+
+newKeys conf = myKeys conf `M.union` keys def conf
 
 main =
   xmonad
@@ -14,5 +24,6 @@ main =
           focusedBorderColor = "#859900",
           layoutHook = myLayoutHook,
           modMask = mod4Mask,
-          terminal = "alacritty"
+          terminal = "alacritty",
+          keys = newKeys
         }
